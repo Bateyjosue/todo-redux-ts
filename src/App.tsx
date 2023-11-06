9e56099b563a852c1fbf515df5e44a886aa40f8e
Bateyjosue <josuebatey19@gmail.com>
2023-11-06 08:38:45 +0200

    feat: render todo and inputForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import InputForm from "./components/InputForm"
+import Todo from "./components/Todo"
 
 function App() {
 
   return (
     <div className="container mx-auto px-8">
       <h1 className="text-5xl font-bold text-gray-500 text-center my-12">Todos</h1>
-      <InputForm/>
+      <InputForm />
+      <Todo />
     </div>
   )
 }
